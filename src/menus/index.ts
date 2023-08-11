cbefdc1ca4ca7456eaacc3dfaf737553a911c62a
wnexous <andreneexus@hotmail.com>
2023-08-11 10:14:05 -0300

    END v.1.2

--- src/menus/index.ts
+++ src/menus/index.ts
@@ -1,34 +1,31 @@
 import { NexusPropsMenuInterface, NexusReturnMenuInterface } from "nexus-wa/global/interfaces/messageController";
-import { menuTeste } from "./testes";
+import { menuIntroduction } from "./introdution";
 
 export default function handle({ message, hooks, menu }: NexusPropsMenuInterface): NexusReturnMenuInterface {
 
-
-    console.log(menu);
-    // save last message 
-    const lastMessage = hooks.dataManager.getData({ token: "lastMessage" }) || { data: "sem mensagens" }
+    // const lastMessage = hooks.dataManager.getData({ token: "lastMessage" }) || { data: "sem mensagens" }
     hooks.dataManager.setData({ token: "lastMessage", data: message.body })
 
 
-    if (message.body == "teste") {
-        hooks.changeMenuByPath({ menuPath: "/testes" })
-        return [
-            { type: "message", content: "digite *teste* para o menu de testes" },
-            { type: "message", content: menuTeste(), messageDelay: 2000 },
-        ]
+    switch (message.body) {
+        case "introdução":
+            hooks.changeMenuByPath({ menuPath: "/introdution" })
+            return [
+                { type: "message", content: menuIntroduction() },
+            ]
 
+        default:
+            return [
+                { type: "message", content: menuIndex() },
+            ]
     }
-
-    return [
-        { type: "message", content: menuIndex() },
-    ]
-
 }
 
-
 export const menuIndex = () => `
-
 *MENU PRINCIPAL*
 
-para ir ao menu de teste, digite *teste*
+Bem vindo ao menu principal
+digite *intrudução* para saber mais sobre o projeto
+
+_nexus bot_ by nexous
 `
